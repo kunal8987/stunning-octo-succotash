1ee2f0fe99cc1b4eca74d9d60d4eccc60bfef779
Kunal Deotale <kdeotale4@gmail.com>
2024-06-08 01:14:13 +0530

    create  auth model

---
+++ Backend/src/Model/Auth.model.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const authSchema = new mongoose.Schema(
+    {
+        userName: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+    },
+    { timestamps: true, versionKey: false }
+);
+
+const Auth = mongoose.model("Auth", authSchema);
+
+module.exports = Auth;
