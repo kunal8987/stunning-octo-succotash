f7ba4d610fcd841b79ed2b12880cf82eaf32e453
Kunal Deotale <kdeotale4@gmail.com>
2024-06-24 21:31:22 +0530

    add pages code and navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,13 @@
+import { Navbar } from "./Componend/Navbar/Navbar";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <>
+      <div className="bg-[#024950]" >
+        <div className="container mx-auto px-5 md:px-10 lg:px-14 xl:px-24">
+          <Navbar />
+        </div>
+      </div>
+    </>
+  );
 }
