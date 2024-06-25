a63e2da99389e00ef56e5eb00106aa04b7237cab
Kunal Deotale <kdeotale4@gmail.com>
2024-06-25 23:19:19 +0530

    fix minor bug

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-import { store } from "./Redux/store";
+import { store } from "./Redux/Store";
 import { Provider } from "react-redux";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
